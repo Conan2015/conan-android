cc6d41e7dacc42faa30415e83fa4de46a061c483
Conan <Conan@birckand.com>
2015-12-03 11:58:27 +0900

    로그 추가

--- MyLocation/app/src/main/java/com/conan/mylocation/MainActivity.java
+++ MyLocation/app/src/main/java/com/conan/mylocation/MainActivity.java
@@ -3,6 +3,7 @@ package com.conan.mylocation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(LOG_TAG, "MainActivity");
 
         Intent intent = new Intent(this, MyLocationActivity.class);
         startActivity(intent);

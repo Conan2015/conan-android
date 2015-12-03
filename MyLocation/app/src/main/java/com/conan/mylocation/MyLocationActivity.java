e3b3e85bfdeb0a19c2bae1a515284af014254518
Conan <Conan@birckand.com>
2015-12-03 11:51:02 +0900

    ...

--- MyLocation/app/src/main/java/com/conan/mylocation/MyLocationActivity.java
+++ MyLocation/app/src/main/java/com/conan/mylocation/MyLocationActivity.java
@@ -2,6 +2,7 @@ package com.conan.mylocation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MyLocationActivity extends AppCompatActivity {
     private final static String LOG_TAG = MyLocationActivity.class.getSimpleName();
@@ -10,5 +11,7 @@ public class MyLocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_location);
+
+        Log.d(LOG_TAG, "onCreate ~~~");
     }
 }
